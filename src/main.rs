f678ed300a4a8e9282d1c6aa7a1a00c54f4938ef
Sibal Hulselmans <Sibal.hulselmans@protonmail.com>
2025-03-09 23:00:13 +0100

    change game name

--- src/main.rs
+++ src/main.rs
@@ -29,7 +29,7 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Assasin".to_string(),
+                        title: "Assassin".to_string(),
                         resolution: Vec2::new(512., 512.).into(),
                         position: WindowPosition::Centered(MonitorSelection::Current),
                         canvas: Some("#bevy".to_string()),
