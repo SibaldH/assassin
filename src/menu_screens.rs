50442b0bd5be01125089e651ac859af0baf9de8c
Sibal Hulselmans <Sibal.hulselmans@protonmail.com>
2025-03-23 22:04:26 +0100

    add placeholder settings page
    change menustate to have a seperate settings type

--- src/menu_screens.rs
+++ src/menu_screens.rs
@@ -15,12 +15,33 @@ impl Plugin for MenuPlugin {
             OnEnter(MenuState::Credits),
             credit_screen.run_if(in_state(GameState::MainMenu)),
         );
+        app.add_systems(
+            OnEnter(MenuState::Settings(SettingsType::General)),
+            settings_screen.run_if(in_state(GameState::MainMenu)),
+        );
+        app.add_systems(
+            OnEnter(MenuState::Settings(SettingsType::Audio)),
+            settings_screen.run_if(in_state(GameState::MainMenu)),
+        );
+        app.add_systems(
+            OnEnter(MenuState::Settings(SettingsType::Controls)),
+            settings_screen.run_if(in_state(GameState::MainMenu)),
+        );
         app.add_systems(Update, button_system.run_if(in_state(GameState::MainMenu)));
         app.add_systems(OnExit(GameState::MainMenu), despawn_menu);
         app.add_systems(OnExit(MenuState::Main), despawn_menu);
-        app.add_systems(OnExit(MenuState::GeneralSettings), despawn_menu);
-        app.add_systems(OnExit(MenuState::AudioSettings), despawn_menu);
-        app.add_systems(OnExit(MenuState::ControlSettings), despawn_menu);
+        app.add_systems(
+            OnExit(MenuState::Settings(SettingsType::General)),
+            despawn_menu,
+        );
+        app.add_systems(
+            OnExit(MenuState::Settings(SettingsType::Audio)),
+            despawn_menu,
+        );
+        app.add_systems(
+            OnExit(MenuState::Settings(SettingsType::Controls)),
+            despawn_menu,
+        );
         app.add_systems(OnExit(MenuState::Credits), despawn_menu);
         app.add_systems(OnEnter(MenuState::Quit), exit_app);
     }
@@ -33,11 +54,7 @@ const HOVERED_BUTTON_COLOR: Color = Color::srgba(0.1, 0.4, 0.1, 0.3);
 #[derive(Component)]
 struct MainScreenUI;
 #[derive(Component)]
-struct GeneralSettingsScreenUI;
-#[derive(Component)]
-struct AudioSettingsScreenUI;
-#[derive(Component)]
-struct ControlSettingsScreenUI;
+struct SettingsScreenUI;
 #[derive(Component)]
 struct CreditScreenUI;
 
@@ -45,17 +62,78 @@ struct CreditScreenUI;
 enum MenuState {
     #[default]
     Main,
-    GeneralSettings,
-    AudioSettings,
-    ControlSettings,
+    Settings(SettingsType),
     Credits,
     Quit,
+}
+
+#[derive(Hash, Eq, PartialEq, Debug, Clone, Default)]
+enum SettingsType {
+    #[default]
+    General,
+    Audio,
+    Controls,
 }
 
 #[derive(Component, Debug)]
 enum NextStateDestination {
     Menu(MenuState),
     Game(GameState),
+}
+
+#[allow(clippy::type_complexity)]
+fn button_system(
+    mut interaction_query: Query<
+        (&Interaction, &mut BackgroundColor, &NextStateDestination),
+        (Changed<Interaction>, With<Button>),
+    >,
+    mut next_menu_state: ResMut<NextState<MenuState>>,
+    mut next_game_state: ResMut<NextState<GameState>>,
+) {
+    for (interaction, mut bg_color, destination) in &mut interaction_query {
+        match *interaction {
+            Interaction::Pressed => {
+                *bg_color = BackgroundColor(PRESSED_BUTTON_COLOR);
+                match destination {
+                    NextStateDestination::Menu(state) => {
+                        next_game_state.set(GameState::default());
+                        next_menu_state.set(state.clone());
+                    }
+                    NextStateDestination::Game(state) => {
+                        next_menu_state.set(MenuState::default());
+                        next_game_state.set(state.clone());
+                    }
+                }
+            }
+            Interaction::Hovered => {
+                *bg_color = BackgroundColor(HOVERED_BUTTON_COLOR);
+            }
+            Interaction::None => {
+                *bg_color = BackgroundColor(NORMAL_BUTTON_COLOR);
+            }
+        }
+    }
+}
+
+#[allow(clippy::type_complexity)]
+fn despawn_menu(
+    mut commands: Commands,
+    query: Query<
+        Entity,
+        Or<(
+            With<MainScreenUI>,
+            With<SettingsScreenUI>,
+            With<CreditScreenUI>,
+        )>,
+    >,
+) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
+
+fn exit_app(mut exit: EventWriter<AppExit>) {
+    exit.send(AppExit::Success);
 }
 
 fn main_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -141,7 +219,7 @@ fn main_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
                             BorderColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
                             BackgroundColor(NORMAL_BUTTON_COLOR),
                             BorderRadius::MAX,
-                            NextStateDestination::Menu(MenuState::GeneralSettings),
+                            NextStateDestination::Menu(MenuState::Settings(SettingsType::General)),
                         ))
                         .with_children(|parent| {
                             parent.spawn((
@@ -302,59 +380,195 @@ fn credit_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
         });
 }
 
-#[allow(clippy::type_complexity)]
-fn button_system(
-    mut interaction_query: Query<
-        (&Interaction, &mut BackgroundColor, &NextStateDestination),
-        (Changed<Interaction>, With<Button>),
-    >,
-    mut next_menu_state: ResMut<NextState<MenuState>>,
-    mut next_game_state: ResMut<NextState<GameState>>,
-) {
-    for (interaction, mut bg_color, destination) in &mut interaction_query {
-        match *interaction {
-            Interaction::Pressed => {
-                *bg_color = BackgroundColor(PRESSED_BUTTON_COLOR);
-                match destination {
-                    NextStateDestination::Menu(state) => {
-                        next_game_state.set(GameState::default());
-                        next_menu_state.set(state.clone());
-                    }
-                    NextStateDestination::Game(state) => {
-                        next_menu_state.set(MenuState::default());
-                        next_game_state.set(state.clone());
-                    }
-                }
-            }
-            Interaction::Hovered => {
-                *bg_color = BackgroundColor(HOVERED_BUTTON_COLOR);
-            }
-            Interaction::None => {
-                *bg_color = BackgroundColor(NORMAL_BUTTON_COLOR);
-            }
-        }
-    }
-}
-
-#[allow(clippy::type_complexity)]
-fn despawn_menu(
+fn settings_screen(
     mut commands: Commands,
-    query: Query<
-        Entity,
-        Or<(
-            With<MainScreenUI>,
-            With<GeneralSettingsScreenUI>,
-            With<AudioSettingsScreenUI>,
-            With<ControlSettingsScreenUI>,
-            With<CreditScreenUI>,
-        )>,
-    >,
+    asset_server: Res<AssetServer>,
+    menu_settings: Res<State<MenuState>>,
 ) {
-    for entity in query.iter() {
-        commands.entity(entity).despawn_recursive();
-    }
-}
+    let font = asset_server.load("fonts/MatrixtypeDisplay-9MyE5.ttf");
 
-fn exit_app(mut exit: EventWriter<AppExit>) {
-    exit.send(AppExit::Success);
+    commands
+        .spawn((
+            Node {
+                width: Val::Percent(70.),
+                height: Val::Percent(70.0),
+                align_items: AlignItems::Center,
+                align_self: AlignSelf::Center,
+                justify_content: JustifyContent::SpaceBetween,
+                justify_self: JustifySelf::Center,
+                flex_direction: FlexDirection::Column,
+                ..default()
+            },
+            BackgroundColor(Color::srgba(1.0, 1.0, 1.0, 0.05)),
+            BorderRadius::all(Val::Px(10.0)),
+            BorderColor(Color::srgb(0.0, 0.0, 0.0)),
+            SettingsScreenUI,
+        ))
+        .with_children(|parent| {
+            parent
+                .spawn(Node {
+                    width: Val::Auto,
+                    height: Val::Percent(10.),
+                    align_items: AlignItems::Center,
+                    justify_content: JustifyContent::SpaceEvenly,
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Auto,
+                                height: Val::Percent(80.),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                padding: UiRect::all(Val::Px(5.)),
+                                ..default()
+                            },
+                            BorderColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            BackgroundColor(NORMAL_BUTTON_COLOR),
+                            BorderRadius::MAX,
+                            NextStateDestination::Menu(MenuState::Settings(SettingsType::General)),
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("General"),
+                                TextFont {
+                                    font: font.clone(),
+                                    font_size: 15.0,
+                                    ..default()
+                                },
+                                TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Auto,
+                                height: Val::Percent(80.),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                padding: UiRect::all(Val::Px(5.)),
+                                ..default()
+                            },
+                            BorderColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            BackgroundColor(NORMAL_BUTTON_COLOR),
+                            BorderRadius::MAX,
+                            NextStateDestination::Menu(MenuState::Settings(SettingsType::Audio)),
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("Audio"),
+                                TextFont {
+                                    font: font.clone(),
+                                    font_size: 15.0,
+                                    ..default()
+                                },
+                                TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            ));
+                        });
+                    parent
+                        .spawn((
+                            Button,
+                            Node {
+                                width: Val::Auto,
+                                height: Val::Percent(80.),
+                                align_items: AlignItems::Center,
+                                justify_content: JustifyContent::Center,
+                                padding: UiRect::all(Val::Px(5.)),
+                                ..default()
+                            },
+                            BorderColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            BackgroundColor(NORMAL_BUTTON_COLOR),
+                            BorderRadius::MAX,
+                            NextStateDestination::Menu(MenuState::Settings(SettingsType::Controls)),
+                        ))
+                        .with_children(|parent| {
+                            parent.spawn((
+                                Text::new("Controls"),
+                                TextFont {
+                                    font: font.clone(),
+                                    font_size: 15.0,
+                                    ..default()
+                                },
+                                TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                            ));
+                        });
+                });
+            parent
+                .spawn(Node {
+                    width: Val::Auto,
+                    height: Val::Percent(90.0),
+                    flex_direction: FlexDirection::Column,
+                    padding: UiRect::all(Val::Px(5.)),
+                    justify_content: JustifyContent::SpaceBetween,
+                    row_gap: Val::Px(5.0),
+                    ..default()
+                })
+                .with_children(|parent| match menu_settings.get() {
+                    MenuState::Settings(SettingsType::General) => {
+                        parent.spawn((
+                            Text::new("General"),
+                            TextFont {
+                                font: font.clone(),
+                                font_size: 50.0,
+                                ..default()
+                            },
+                            TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                        ));
+                    }
+                    MenuState::Settings(SettingsType::Audio) => {
+                        parent.spawn((
+                            Text::new("Audio"),
+                            TextFont {
+                                font: font.clone(),
+                                font_size: 50.0,
+                                ..default()
+                            },
+                            TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                        ));
+                    }
+                    MenuState::Settings(SettingsType::Controls) => {
+                        parent.spawn((
+                            Text::new("Controls"),
+                            TextFont {
+                                font: font.clone(),
+                                font_size: 50.0,
+                                ..default()
+                            },
+                            TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                        ));
+                    }
+                    _ => (),
+                });
+            parent
+                .spawn((
+                    Node {
+                        width: Val::Px(80.),
+                        height: Val::Px(30.),
+                        align_self: AlignSelf::Start,
+                        justify_self: JustifySelf::Center,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        margin: UiRect::all(Val::Px(5.)),
+                        ..default()
+                    },
+                    Button,
+                    BackgroundColor(NORMAL_BUTTON_COLOR),
+                    BorderRadius::MAX,
+                    NextStateDestination::Menu(MenuState::Main),
+                ))
+                .with_children(|parent| {
+                    parent.spawn((
+                        Text::new("Back"),
+                        TextFont {
+                            font: font.clone(),
+                            font_size: 15.0,
+                            ..default()
+                        },
+                        TextColor(Color::srgba(1.0, 1.0, 1.0, 1.0)),
+                    ));
+                });
+        });
 }
